15af740e75a128dac47a215945261d4b40a42913
CJ <155175890+jaglac@users.noreply.github.com>
2026-06-20 11:27:55 -0400

    patches to IMAP4WithTimeout and IMAPClient for python 3.14 compatibility

--- imapclient/imap4.py
+++ imapclient/imap4.py
@@ -7,21 +7,31 @@ import socket
 from typing import Optional
 
 
+# class IMAP4WithTimeout(imaplib.IMAP4):
+#     def __init__(self, address: str, port: int, timeout: Optional[float]) -> None:
+#         self._timeout = timeout
+#         imaplib.IMAP4.__init__(self, address, port)
+
+#     def open(
+#         self, host: str = "", port: int = 143, timeout: Optional[float] = None
+#     ) -> None:
+#         # This is overridden to make it consistent across Python versions.
+#         self.host = host
+#         self.port = port
+#         self.sock = self._create_socket(timeout)
+#         self.file = self.sock.makefile("rb")
+
+#     def _create_socket(self, timeout: Optional[float] = None) -> socket.socket:
+#         return socket.create_connection(
+#             (self.host, self.port), timeout if timeout is not None else self._timeout
+#         )
+
+
 class IMAP4WithTimeout(imaplib.IMAP4):
-    def __init__(self, address: str, port: int, timeout: Optional[float]) -> None:
+    def __init__(self, host: str = '', port: int = 143, timeout: float | None = None) -> None:
         self._timeout = timeout
-        imaplib.IMAP4.__init__(self, address, port)
+        super().__init__(host=host, port=port, timeout=timeout)
 
-    def open(
-        self, host: str = "", port: int = 143, timeout: Optional[float] = None
-    ) -> None:
-        # This is overridden to make it consistent across Python versions.
-        self.host = host
-        self.port = port
-        self.sock = self._create_socket(timeout)
-        self.file = self.sock.makefile("rb")
-
-    def _create_socket(self, timeout: Optional[float] = None) -> socket.socket:
-        return socket.create_connection(
-            (self.host, self.port), timeout if timeout is not None else self._timeout
-        )
+    def _create_socket(self, timeout: float | None = None) -> socket.socket:
+        timeout = timeout if timeout is not None else self._timeout
+        return super()._create_socket(timeout=timeout)
